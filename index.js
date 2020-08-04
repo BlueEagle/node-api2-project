844fa2a5eb3baa1992b91fa073af656fee9fa23f
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-08-04 13:37:45 -0600

    added router

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
-
 const server = express();
 
+const postsRouter = require("./routers/posts-router");
+
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Try endpoints starting with: /api/posts");
+});
+
+server.use("/api/posts", postsRouter);
 
 server.listen(4000, () =>
   console.log(`Server listening at http://localhost:4000`)
